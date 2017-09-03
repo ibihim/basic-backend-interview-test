6428032ec730a4f4cb2bfff90a1b8d2da21c9993
Krzysztof Ostrowski <ibihim@posteo.de>
2017-09-03 22:03:36 +0200

    task 2: in progress. used wrong api, lol

--- tests/integration/lib/nasa/wrapper-test.js
+++ tests/integration/lib/nasa/wrapper-test.js
@@ -1,48 +1,89 @@
 'use strict';
 
+const moment = require('moment');
 const expect = require('chai').expect;
 const _ = require('lodash/fp');
 
 const ROOT = '../../../..';
-const { neo } = require(`${ ROOT }/lib/nasa/wrapper`);
+const { neo, feed } = require(`${ ROOT }/lib/nasa/wrapper`);
 
-const getNearEarthObjects = _.pick('near_earth_objects');
-const checkNeoListLength = expectedLength => neos =>
-    expect(neos).to.have.lengthOf.above(expectedLength);
+const API_KEY = process.env.API_KEY;
+const DATE_FORMAT = 'YYYY-MM-DD';
+
+const getNearEarthObjects = resObj => resObj.near_earth_objects;
+const getNeoReferenceId = obj => obj.neo_reference_id;
+
+const disjointLists = ([ listA, listB ]) => expect(listA).to.not.have.members(listB);
+const sameListAs = listA => listB => expect(listB.sort()).to.eql(listA.sort());
+const checkListLength = expectedLength => list => expect(list).to.have.lengthOf(expectedLength);
 
 describe('wrapper', () => {
-    it('should return list of asteroids', (done) => {
-        const minimalExpectedAmountOfAsteroids = 100;
-        const checkMinimalLength = checkNeoListLength(minimalExpectedAmountOfAsteroids);
 
-        neo.get()
-            .then(getNearEarthObjects)
-            .then(checkMinimalLength)
-            .then(() => done())
-            .catch(done);
+    before(() => expect(API_KEY, 'API_KEY is not set in ENV').to.not.be.empty);
+
+    describe('for neo', () => {
+        it('should return list of asteroids', (done) => {
+            const DEFAULT_LENGTH = 10;
+            const checkMinimalLength = checkListLength(DEFAULT_LENGTH);
+
+            neo.get()
+               .then(getNearEarthObjects)
+               .then(checkMinimalLength)
+               .then(() => done())
+               .catch(done);
+        });
+
+        it('should return different pages of asteroids', (done) => {
+            const firstPage = 0;
+            const secondPage = 1;
+            const pageSize = 10;
+
+
+            const getNeoIdsFor = (page, size) => neo.get(page, size)
+                                                    .then(getNearEarthObjects)
+                                                    .then(_.tap(checkListLength(pageSize)))
+                                                    .then(_.map(getNeoReferenceId));
+
+            Promise.all([
+                       getNeoIdsFor(firstPage, pageSize),
+                       getNeoIdsFor(secondPage, pageSize)
+                   ])
+                   .then(disjointLists)
+                   .then(() => done())
+                   .catch(done);
+
+        });
     });
 
-    it('should return different pages of asteroids', (done) => {
-        const firstPage = 0;
-        const secondPage = 1;
-        const pageSize = 10;
+    describe('for feed', () => {
+        it('should return data of the last 3 days', (done) => {
+            const subtractDays = dayCount =>
+                moment().utc()
+                        .subtract(dayCount, 'days')
+                        .format(DATE_FORMAT);
+            const TODAY = subtractDays(0);
+            const YESTERDAY = subtractDays(1);
+            const DAY_BEFORE_YESTERDAY = subtractDays(2);
 
-        const pickId = _.pick('neo_reference_id');
-        const disjointLists = ([ listA, listB ]) =>
-            expect(listA).to.not.have.members(listB);
+            feed.get(DAY_BEFORE_YESTERDAY)
+                .then(getNearEarthObjects)
+                .then(_.keys)
+                .then(sameListAs([ TODAY, YESTERDAY, DAY_BEFORE_YESTERDAY ]))
+                .then(() => done())
+                .catch(done);
 
-        const getNeoIdsFor = (page, size) => neo.get(page, size)
-            .then(getNearEarthObjects)
-            .then(_.tap(checkNeoListLength(pageSize)))
-            .then(_.map(pickId));
+        });
 
-        Promise.all([
-            getNeoIdsFor(firstPage, pageSize),
-            getNeoIdsFor(secondPage, pageSize)
-        ])
-            .then(disjointLists)
-            .then(() => done)
-            .catch(done);
+        it.skip('should return data for the last month', (done) => {
+            const lastMonth = moment().utc().subtract(1, 'month').month();
 
+            feed.get(lastMonth)
+                .then(getNearEarthObjects)
+                .then(_.keys)
+                .then(sameListAs([ TODAY, YESTERDAY, DAY_BEFORE_YESTERDAY ]))
+                .then(() => done())
+                .catch(done);
+        });
     });
+
 });
