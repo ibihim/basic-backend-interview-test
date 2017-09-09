f026a8ca800e3f42a67c5e055596de95641893e4
Krzysztof Ostrowski <ibihim@posteo.de>
2017-09-09 14:01:02 +0200

    minor cleanup

--- controllers/api/neo/hazardous.js
+++ controllers/api/neo/hazardous.js
@@ -9,12 +9,10 @@ const { findNeo } = require(`${ ROOT }/lib/neo`);
 
 // TODO pagination / HATEOAS
 router.get('/', (req, res) => {
-    // TODO this will explode like shit, PAGINATION!!!!
     const respond = neos => res.status(HttpStatus.OK)
                        .type('application/json')
                        .json(neos);
 
-    // TODO clean up all .exec()!
     findNeo({ isHazardous: true }).then(respond);
 });
 
